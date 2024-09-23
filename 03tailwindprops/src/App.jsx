9769aa47bbb0793f7e6c1ca04811e252c7150ced
Gangadhar-H <gangadharhaladodi@gmail.com>
2024-09-23 22:09:30 +0530

    tailwind and props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Card from './components/Card';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card channel='chai' price={24} />
+      <Card channel='code' />
+    </>
+  )
+}
+
+export default App
